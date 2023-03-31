0362983a88f54d32b4c7087204d0b10f166a12b5
aditya <kumarat1999@gmail.com>
2023-03-31 16:03:39 +0530

    initial commit

---
+++ BreakLoopDemo.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class BreakLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0;i<10;i++)
+		{
+			if(i==4)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ TypeCastingDemo.java
@@ -0,0 +1,22 @@
+package mypackage;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c ='A';
+		// TypeCasting
+		int b =c;
+		float d = b;
+		double e = b;
+		long f = b;
+		System.out.println("b:"+b);
+		System.out.println("d:"+d);
+		System.out.println("e:"+e);
+		System.out.println("f:"+f);
+		
+		
+
+	}
+
+}
